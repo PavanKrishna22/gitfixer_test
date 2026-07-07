4a4ea5c9852af5e4b14aa25044e4ef85d97c48ef
Pavan Krishna. O <102010168+PavanKrishna22@users.noreply.github.com>
2026-07-07 23:29:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from calculator import add, subtract, multiply, divide
+
+
+def main():
+    a = 10
+    b = 5
+
+    print(f"Add: {add(a, b)}")
+    print(f"Subtract: {subtract(a, b)}")
+    print(f"Multiply: {multiply(a, b)}")
+    print(f"Divide: {divide(a, b)}")
+
+
+if __name__ == "__main__":
+    main()
